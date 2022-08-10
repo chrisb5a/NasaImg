f072a66810995e242ec5c8ebe7e8c5ad9b46897f
CHRISTIAN BEYNIS <christianbeynis@CHRISTIANs-MacBook-Pro.local>
2022-08-10 16:52:41 -0400

    UI test Fine with swift and bridge

--- Nasa_images/Nasa_imagesTests/Nasa_imagesTests.swift
+++ Nasa_images/Nasa_imagesTests/Nasa_imagesTests.swift
@@ -3,7 +3,52 @@
 //  Nasa_imagesTests
 //
 //  Created by CHRISTIAN BEYNIS on 8/8/22.
-//
+
+
+import XCTest
+@testable import Nasa_images
+
+
+class DigimonTests: XCTestCase {
+
+    var viewModel: NasaViewModel?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        self.viewModel = NasaViewModel(networkManager: NetworkManager())
+    }
+
+    override func tearDownWithError() throws {
+        self.viewModel = nil
+        try super.tearDownWithError()
+    }
+    func testRequestModelDataSuccess() {
+        // Arrange
+        let expectation = XCTestExpectation(description: "Successfully retrieving Models")
+        
+        // Act
+        self.viewModel?.bind {
+            expectation.fulfill()
+        }
+        self.viewModel?.fetchNasa()
+        wait(for: [expectation], timeout: 45)
+        
+        //wait(for: [expectation], timeout: 1) => Timeout Failure
+        // Assert
+        //XCTAssertEqual(self.viewModel?.count, 209)
+        XCTAssertEqual(self.viewModel?.title(for: 0), "The Full Moon of 2021")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
+
+// Below is always succeeding test 
 
 import XCTest
 @testable import Nasa_images
